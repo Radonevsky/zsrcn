289a023ba6a76dcf29b7c623392568b0827cd90a
radonevsky <radonevsky@yandex.ru>
2023-08-23 23:21:48 +0400

    add: show more button

--- resources/js/views/News.vue
+++ resources/js/views/News.vue
@@ -1,10 +1,17 @@
 <script setup>
 import PageTitle from "../components/PageTitle.vue";
+import ShowMoreButton from "../components/ShowMoreButton.vue";
+import ContentContainer from "../layouts/ContentContainer.vue";
 </script>
 
 <template>
     <div>
         <PageTitle title='Наши новости' bgClass='bg-orange'/>
+        <div class='pt-[77px] pb-[60px]'>
+            <ContentContainer>
+                <ShowMoreButton text='показать еще' class='mx-auto'/>
+            </ContentContainer>
+        </div>
     </div>
 </template>
 
