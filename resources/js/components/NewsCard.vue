a88cd443098f538fe569bf00ba252fa37afeeb67
radonevsky <radonevsky@yandex.ru>
2023-08-28 20:36:33 +0400

    add: news card scroll, custom scrollbar

--- resources/js/components/NewsCard.vue
+++ resources/js/components/NewsCard.vue
@@ -12,13 +12,13 @@ const props = defineProps({
 </script>
 
 <template>
-<article class='flex gap-[65px] max-w-[1028px] pt-[50px] pb-[80px] px-[50px] rounded-[30px] self-center' :class='props.bgClass'>
+<article class='flex gap-[65px] h-[490px] max-w-[1028px] pt-[50px] pb-[80px] px-[50px] rounded-[30px] self-center' :class='props.bgClass'>
     <img :src="getImgUrl(props.imgPath)" alt='photo' class='max-w-[340px] max-h-[350px]'>
     <div class='flex flex-col gap-[40px] text-white'>
         <h6 class='font-roboto700 text-[30px] leading-[35px]'>
             {{ props.title }}
         </h6>
-        <div>
+        <div class='overflow-auto custom-scrollbar'>
             <p class='font-roboto500 text-[15px] leading-[18px]'>
                 {{ props.text }}
             </p>
