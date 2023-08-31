09335bbf9c1876ed84c77c9b6b276320f05d81ef
radonevsky <radonevsky@yandex.ru>
2023-08-31 10:24:10 +0400

    add: input title new article card

--- resources/js/components/NewsCard.vue
+++ resources/js/components/NewsCard.vue
@@ -16,12 +16,14 @@ const props = defineProps({
 </script>
 
 <template>
-<article class='flex gap-[65px] h-[490px] max-w-[1028px] pt-[50px] pb-[80px] px-[50px] rounded-[30px] self-center'
+<article class='flex gap-[65px] h-[490px] max-w-[1028px] pt-[50px] pb-[80px] px-[50px] rounded-[30px] self-center w-full'
          :class='create ? "bg-bggray" : props.bgClass'>
     <div v-if='create' class='dropzone w-[330px] h-[350px] border-light-orange border-2 border-dashed'></div>
     <img v-if='!create' :src="getImgUrl(props.imgPath)" alt='photo' class='max-w-[340px] max-h-[350px]'>
-    <div class='flex flex-col gap-[40px] text-white'>
-        <h6 class='font-roboto700 text-[30px] leading-[35px]'>
+    <div class='flex flex-col gap-[40px] text-white grow'>
+        <input v-if='create' type='text' class='bg-bggray p-[5px] border-white border-[1px] rounded-[10px] outline-0
+            py-[15px] focus:shadow-around focus:border-orange focus:bg-bluebg text-[30px] font-roboto700'>
+        <h6 v-if='!create' class='font-roboto700 text-[30px] leading-[35px]'>
             {{ props.title }}
         </h6>
         <div class='overflow-auto custom-scrollbar'>
