2a42cdada008971a277c98a625ac235c17165b84
radonevsky <radonevsky@gmail.com>
2024-02-10 17:52:07 +0400

    del: empty lines

--- resources/js/components/DownloadDocButton.vue
+++ resources/js/components/DownloadDocButton.vue
@@ -9,9 +9,7 @@ import DownloadDocIcon from "./icons/downloadDocIcon.vue";
         <div class="w-[32px] flex items-center">
             <download-doc-icon></download-doc-icon>
         </div>
-
         <p class="hover:underline"><slot></slot></p>
-
     </div>
 </template>
 
