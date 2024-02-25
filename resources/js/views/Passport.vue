400fb71dfa759a5ac45e4ecadedc8f65450d75d9
radonevsky <radonevsky@gmail.com>
2024-02-25 15:01:14 +0400

    add: passports

--- resources/js/views/Passport.vue
+++ resources/js/views/Passport.vue
@@ -1,12 +1,32 @@
 <script setup>
 
 import ContentContainer from "../layouts/ContentContainer.vue";
+import {useRouter} from "vue-router";
+import useCommon from "../use/common.js";
+import {ref} from "vue";
+import DocumentDownloadUpload from "../components/DocumentDownloadUpload.vue";
+const router = useRouter();
+const { documentsScrollUp } = useCommon()
+const routerName = ref('center-passport-2023')
 
+router.afterEach((to) => {
+    console.log(11)
+    documentsScrollUp()
+    routerName.value = to.name
+});
 </script>
 
 <template>
     <ContentContainer>
-        Passport
+        <div v-if="routerName === 'center-passport-2023'">
+            <document-download-upload name="Паспорт учреждения 2023г." type="passport_2023"></document-download-upload>
+        </div>
+        <div v-if="routerName === 'center-passport-2021'">
+            <document-download-upload name="Паспорт учреждения 2021г." type="passport_2021"></document-download-upload>
+        </div>
+        <div v-if="routerName === 'center-passport-2020'">
+            <document-download-upload name="Паспорт учреждения 2020г." type="passport_2020"></document-download-upload>
+        </div>
     </ContentContainer>
 </template>
 
