a20542e295d2cce3635fc4ecadef60ed4a9a39cc
radonevsky <radonevsky@gmail.com>
2024-02-28 23:30:47 +0400

    add: adding button

---
+++ resources/js/views/AddButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import PlusIcon from "../components/icons/plusIcon.vue";
+</script>
+
+<template>
+    <button
+        class='mb-[30px] flex mx-auto gap-[5px] hover:cursor-pointer'
+        @click='$emit("click")'>
+                    <span class='font-roboto700 leading-[23px] text-tblue-light uppercase'>
+                        <slot></slot>
+                    </span>
+        <plusIcon></plusIcon>
+    </button>
+</template>
+
+<style scoped>
+
+</style>
